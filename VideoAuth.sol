b819b8a5fbd8527112c30ce0578a01f8ac85714a
[Jason Cadahia] <[jcadahia5@gmail.com]>
2020-04-08 15:47:57 -0500

    Couldn't figure out how to place it on a different branch :(

---
+++ VideoAuth.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.10;
+
+//needs to receive and store original video hash and ID
+//needs to receive requests for video authentication (input: hash)
+//needs to compare received hash with stored hashes
+//needs to output ID of original video if a matching hash is found
+//needs to output message if no matching hash is found
+contract VideoAuth {
+    constructor() public {
+    }
+}
